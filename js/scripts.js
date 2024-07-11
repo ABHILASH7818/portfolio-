3efaea08ee410928d704890c0e25142020281fbd
ABHILASH7818 <abhithiruvamana@gmail.com>
2024-07-11 20:57:40 +0530

    final

---
+++ js/scripts.js
@@ -0,0 +1,121 @@
+/* Description: Custom JS file */
+
+
+(function($) {
+    "use strict"; 
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
+
+/* contact form */
+
+
+
+    const form = document.getElementById('contactForm');
+    const name = document.getElementById('cname');
+    const email = document.getElementById('cemail'); 
+    const message = document.getElementById('cmessage');
+    const nameError = document.getElementById('name_error');
+    const emailError = document.getElementById('email_error'); 
+    const messageError = document.getElementById('message_error');
+    const emailCheck = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    form.addEventListener('submit', (e) => {
+  
+  // Name validation
+    if (name.value === '' || name.value == null) {
+        e.preventDefault();
+        nameError.innerHTML = "Name is required";
+    
+     } else {
+        nameError.innerHTML = "";
+    }
+
+  // Email validation
+    if (!email.value.match(emailCheck)) {
+        e.preventDefault();
+        emailError.innerHTML = "Valid Email is required";
+   
+    } else {
+        emailError.innerHTML = "";
+    }
+
+  // Message validation
+    if (message.value === ''|| message.value == null) {
+        e.preventDefault();
+        messageError.innerHTML = "Message is required";
+    
+    } else {
+        messageError.innerHTML = "";
+     }
+    });
